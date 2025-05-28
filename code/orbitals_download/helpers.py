67716efae6b62ab9b66bb5a6fd0a891568c2ada8
Lyosef789 <lidiyayosefah78@gmail.com>
2025-05-28 16:41:20 -0400

    updates

---
+++ code/orbitals_download/helpers.py
@@ -0,0 +1,84 @@
+import datetime
+import numpy as np
+import astropy.units as u
+from astropy.coordinates import SkyCoord
+from scipy.interpolate import interp1d 
+import sunpy.coordinates
+
+def gen_dt_arr(dt_init,dt_final,cadence_days=1) :
+    """
+    'Generate Datetime Array'
+    Get array of datetime.datetime from {dt_init} to {dt_final} every 
+    {cadence_days} days
+    """
+    dt_list = []
+    while dt_init < dt_final :
+        dt_list.append(dt_init)
+        dt_init += datetime.timedelta(days=cadence_days)
+    return np.array(dt_list)
+
+def datetime2unix(dt_arr) :
+    """Convert 1D array of `datetime.datetime` to unix timestamps"""
+    return np.array([dt.timestamp() for dt in dt_arr])
+def unix2datetime(ut_arr) : 
+    """Convert 1D array of unix timestamps (float) to `datetime.datetime`"""
+    return np.array([datetime.datetime.utcfromtimestamp(ut) for ut in ut_arr])
+def interp_trajectory(dt_in, trajectory_in, dt_out) :
+    trajectory_in.representation_type="spherical"
+    radius_in = trajectory_in.radius.to("R_sun").value
+    lon_in = trajectory_in.lon.to("deg").value
+    lat_in = trajectory_in.lat.to("deg").value
+    disconts = np.where(np.abs(np.diff(lon_in)) > 60)[0]
+    nan_inds = []
+    for discont in disconts: 
+        nan_inds.append(discont)
+        nan_inds.append(discont+1)
+    radius_in[nan_inds] = np.nan
+    lon_in[nan_inds] = np.nan
+    lat_in[nan_inds] = np.nan
+    return SkyCoord(
+        radius=interp1d(datetime2unix(dt_in),radius_in,bounds_error=False)(datetime2unix(dt_out))*u.R_sun,
+        lon=interp1d(datetime2unix(dt_in),lon_in,bounds_error=False)(datetime2unix(dt_out))*u.deg,
+        lat=interp1d(datetime2unix(dt_in),lat_in,bounds_error=False)(datetime2unix(dt_out))*u.deg,        
+        representation_type="spherical",
+        frame=sunpy.coordinates.HeliographicCarrington,
+        obstime=dt_out)
+@u.quantity_input
+def delta_long(r:u.R_sun,
+               r_inner=2.5*u.R_sun,
+               vsw=360.*u.km/u.s,
+               omega_sun=14.713*u.deg/u.d,
+               ):
+    """ 
+    Ballistic longitudinal shift of a Parker spiral connecting two
+    points at radius r and r_inner, for a solar wind speed vsw. Solar
+    rotation rate is also tunable
+    """
+    return (omega_sun * (r - r_inner) / vsw).to("deg")
+
+def ballistically_project(skycoord,r_inner = 2.5*u.R_sun, vr_arr=None) :
+    """
+    Given a `SkyCoord` of a spacecraft trajectory in the Carrington frame,
+    with `representation_type="spherical"`, and optionally an array of
+    measured solar wind speeds at the same time intervals of the trajectory,
+    return a SkyCoord for the trajectory ballistically projected down to 
+    `r_inner` via a Parker spiral of the appropriate curvature. When `vr_arr`
+    is not supplied, assumes wind speed is everywhere 360 km/s
+    """
+    if skycoord.representation_type != "spherical" :
+        skycoord.representation_type="spherical"
+    if vr_arr is None : vr_arr = np.ones(len(skycoord))*360*u.km/u.s
+    lons_shifted = skycoord.lon + delta_long(skycoord.radius,
+                                             r_inner=r_inner,
+                                             vsw=vr_arr
+                                            )
+    return SkyCoord(
+        lon = lons_shifted, 
+        lat = skycoord.lat,
+        radius = r_inner * np.ones(len(skycoord)),
+        representation_type="spherical",
+        frame = skycoord.frame
+    )
+
+def roll_to_180(lons) : 
+    return ((lons + 180) % 360)-180
